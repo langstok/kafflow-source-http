7e1a4b1e710f947d9731fc1a9e1c694416319895
Sander Puts <putssander@gmail.com>
2017-12-23 22:10:51 +0100

    upgrade to es6

--- src/main/java/com/langstok/nlp/httpnaf/configuration/ElasticSearchConfiguration.java
+++ src/main/java/com/langstok/nlp/httpnaf/configuration/ElasticSearchConfiguration.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -18,30 +18,30 @@ import java.net.UnknownHostException;
 @EnableConfigurationProperties(ElasticProperties.class)
 public class ElasticSearchConfiguration {
 
-	private final static Logger logger = Logger.getLogger(ElasticSearchConfiguration.class);
+    private final static Logger logger = Logger.getLogger(ElasticSearchConfiguration.class);
 
-	private final ElasticProperties elasticProperties;
+    private final ElasticProperties elasticProperties;
 
-	public ElasticSearchConfiguration(ElasticProperties elasticProperties){
-		this.elasticProperties = elasticProperties;
-	}
+    public ElasticSearchConfiguration(ElasticProperties elasticProperties){
+        this.elasticProperties = elasticProperties;
+    }
 
-	@Bean
-	public Client client() throws UnknownHostException {
-		if(elasticProperties.isEnabled()){
-			logger.info("Init elasticsearch: " + elasticProperties );
-			Settings settings = Settings.builder()
-			        .put("cluster.name", elasticProperties.getClusterName()).build();
-			
-			TransportClient client = new PreBuiltTransportClient(settings)
-			        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(
-							elasticProperties.getHost()),
-			        		elasticProperties.getPort()));
-			return client;
-		}
-		else{
-			logger.info("ElasticSearch disabled");
-			return null;
-		}
-	}
+    @Bean
+    public Client client() throws UnknownHostException {
+        if(elasticProperties.isEnabled()){
+            logger.info("Init elasticsearch: " + elasticProperties );
+            Settings settings = Settings.builder()
+                    .put("cluster.name", elasticProperties.getClusterName()).build();
+
+            TransportClient client = new PreBuiltTransportClient(settings)
+                    .addTransportAddress(new TransportAddress(InetAddress.getByName(
+                            elasticProperties.getHost()),
+                            elasticProperties.getPort()));
+            return client;
+        }
+        else{
+            logger.info("ElasticSearch disabled");
+            return null;
+        }
+    }
 }
